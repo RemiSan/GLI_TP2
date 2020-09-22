fd70adf406fbc3f6f062b37db92fd18208a079f9
Viotty Remi <remi.viotty@etudiant.univ-rennes1.fr>
2020-09-22 18:18:00 +0200

    Q3 done

---
+++ pokedemo/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchBarComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
